7b6e92fe96685ef004021de7992134d900067d97
ZabihullahRPL11 <zaachaf@gmail.com>
2024-11-03 22:12:35 +0700

    push pertama

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/223040119_PW2024');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', '223040119_PW2024');
